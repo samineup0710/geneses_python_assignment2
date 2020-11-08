9bfe81be4755f25d853d8fc2e7e0c31572d798d3
SAMIDHA <samida226077@gmail.com>
2020-11-08 09:26:02 +0545

    converting strings from users to uppercase and lowercase.

---
+++ strtoupandlow.py
@@ -0,0 +1,10 @@
+"""ask input from user"""
+input_str = input("enter input in strings:")
+
+"""converting to uppercase"""
+to_upper = input_str.upper()
+print("The strings in uppercase is:", to_upper)
+
+"""converting to lowercase"""
+to_lower = inputs_str.lower()
+print("The strings in lowercase is:", to_lower)
